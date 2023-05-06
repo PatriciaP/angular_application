1684e1ffbd9e1ba7eea15dfa2b1a299cb5681e34
Patricia Amarante <patricia.amarantes@ntconsult.com.br>
2023-05-06 20:17:59 -0300

    Creation of the component ManagementList

---
+++ angular-app/src/app/management-list/management-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-management-list',
+  templateUrl: './management-list.component.html',
+  styleUrls: ['./management-list.component.css']
+})
+export class ManagementListComponent {
+
+}
